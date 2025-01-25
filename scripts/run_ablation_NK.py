542881f0c362636c1158c5df11e7cfb0bf49e0c4
mahakaran <mahakaran.s.sandhu@gmail.com>
2025-01-25 07:04:45 +0000

    x

--- scripts/run_ablation_NK.py
+++ scripts/run_ablation_NK.py
@@ -7,29 +7,45 @@ from benchmarking.interpolation import interpolation_test
 
 from benchmarking.file_proc import make_landscape_data_dicts
 
+import time
+import random
+
 
 ALPHABET = 'ACDEFG'
 SEQ_LEN  = 6
-
+random.seed(10) 
+N_REPLICATES = 4 
 
 #ablation NK 
 def main(): 
     print('Loading data and hyperparameter optimisation.')
-    model_dict, data_dict = make_landscape_data_dicts(data_dir='./data/nk_landscapes/', 
-                                                    model_dir='./hyperopt/ohe/nk_landscape_hparams/', 
-                                                    alphabet=ALPHABET)
+    model_dict, data_dict = make_landscape_data_dicts(data_dir='../data/nk_landscapes/', 
+                                                      model_dir='../hyperopt/ohe/nk_landscape_hparams/', 
+                                                      alphabet=ALPHABET)
+    
     
 
+    data_dict_repl = dict(random.sample(list(data_dict.items()), N_REPLICATES))
+
+    print(data_dict_repl)
     print('Training and testing models.')
+
+    t1 = time.time()
+    """
     ablation_results = ablation_test(model_dict=model_dict, 
                                     landscape_dict=data_dict, 
                                     sequence_len=6, 
                                     alphabet_size=len(ALPHABET), 
                                     file_name='ablation_results',
-                                    n_epochs=100, 
+                                    n_epochs=1, 
                                     patience=10,
                                     min_delta=1e-5
                                     )
+    t2 = time.time()
+    
+    with open('ablation_time.log', 'w') as file: 
+        file.write("Time taken: {}".format(t2-t1))       
+    """  
 
 if __name__ == "__main__": 
     main()
